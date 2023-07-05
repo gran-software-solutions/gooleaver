ca2db89df72d348c3e7b57f33baf5a54e8e3d6f2
Dusan Odalovic <dusan.odalovic@gransoftware.de>
2023-07-05 19:19:46 +0200

    #861n11b44: Add sample backend code (vert.x kotlin)

---
+++ backend/app/README.md
@@ -0,0 +1,12 @@
+# gooleaver backend
+
+## How to run
+
+```shell
+./gradlew run
+```
+
+The server will be running on port 8888, and expose two endpoints:
+
+* `/health/liveness`
+* `/health/readiness`
---
+++ backend/app/src/main/kotlin/de/gransoftware/gooleaver/MainVerticle.kt
@@ -0,0 +1,30 @@
+package de.gransoftware.gooleaver
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Promise
+import io.vertx.ext.web.Router
+
+class MainVerticle : AbstractVerticle() {
+
+  override fun start(startPromise: Promise<Void>) {
+    val router = Router.router(vertx)
+    router.get("/health/liveness").handler { ctx ->
+      ctx.response().end("OK")
+    }
+    router.get("/health/readiness").handler { ctx ->
+      ctx.response().end("OK")
+    }
+
+    vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .listen(8888) { http ->
+        if (http.succeeded()) {
+          startPromise.complete()
+          println("HTTP server started on port 8888")
+        } else {
+          startPromise.fail(http.cause());
+        }
+      }
+  }
+}
