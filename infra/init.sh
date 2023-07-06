e57ebfa3abca4585431c30756be511672a2198b1
Honey Malviya <hi@honeymalviya.com>
2023-07-06 10:53:05 +0200

    update terraform init script

--- infra/init.sh
+++ infra/init.sh
@@ -9,18 +9,18 @@ KEY_VAULT_NAME=tfstate-key-vault-$RANDOM
 LOCATION=germanywestcentral
 
 # Create resource group
-echo "Create a resource group:"
+echo "Creating a resource group..."
 az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
 
 # Create storage account - SKU Standard Locally Redundant Storage
-echo "Create a storage account:"
+echo "Creating a storage account..."
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
 # Create blob container
-echo "Create a blob container:"
-az storage container create --auth-mode login --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+echo "Creating a blob container..."
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --default-action Allow
 
-echo "Creating the key vault:"
+echo "Creating the key vault..."
 az keyvault create --name $KEY_VAULT_NAME --resource-group $RESOURCE_GROUP_NAME --location $LOCATION
 
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
