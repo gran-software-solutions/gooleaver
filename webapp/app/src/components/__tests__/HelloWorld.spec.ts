9db140e4a925b902643c7a17edbd4727c04df213
Honey Malviya <hi@honeymalviya.com>
2023-07-07 11:51:41 +0200

    test fail ci

--- webapp/app/src/components/__tests__/HelloWorld.spec.ts
+++ webapp/app/src/components/__tests__/HelloWorld.spec.ts
@@ -1,6 +1,5 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it } from 'vitest'
 
-import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
