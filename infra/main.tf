fadf67358694646b099e0ca61b069f385896e933
Greg Glazewski <g.glazewski@gmail.com>
2023-07-07 10:51:31 +0200

    try to configure CI and publish image to ACR

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     azurerm = { }
+    azuread = { }
   }
 
   backend "azurerm" { }
@@ -10,7 +11,23 @@ provider "azurerm" {
   features {}
 }
 
+provider "azuread" { }
+
 resource "azurerm_resource_group" "rg_gooleaver" {
   name     = "rgGooleaver"
-  location = "Germany West Central"
+  location = var.location
+}
+
+resource "random_integer" "random_num" {
+  min = 1
+  max = 50000
+}
+
+resource "azurerm_key_vault" "gooleaver_keyvault" {
+  location            = var.location
+  name                = format("%s%s", "gooleaver-keyvault", random_integer.random_num.result)
+  resource_group_name = azurerm_resource_group.rg_gooleaver.name
+  sku_name            = "standard"
+  tenant_id           = var.tenant_id
+  enable_rbac_authorization = true
 }
